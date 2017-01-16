9c74d1bdf1fc5889307bc05c4994ef348a74c36c
Konstantin Läufer <klaeufer@gmail.com>
2017-01-16 12:54:11 -0600

    auto-reformatted sources
    Signed-off-by: Konstantin Läufer <klaeufer@gmail.com>

--- src/test/scala/Benchmark.scala
+++ src/test/scala/Benchmark.scala
@@ -5,23 +5,23 @@ object Benchmark extends Bench.LocalTime {
   val sizes = Gen.exponential("size")(10, 10000, 10)
 
   measure method "+(single arg)" in {
-    using (sizes) in { size =>
-      (1 to size).foldLeft(Map.empty[Int, Unit]) { (m, i) => 
+    using(sizes) in { size =>
+      (1 to size).foldLeft(Map.empty[Int, Unit]) { (m, i) =>
         m + (i -> (())) + (i + size -> (()))
       }
     }
   }
 
   measure method "+(two args)" in {
-    using (sizes) in { size =>
-      (1 to size).foldLeft(Map.empty[Int, Unit]) { (m, i) => 
+    using(sizes) in { size =>
+      (1 to size).foldLeft(Map.empty[Int, Unit]) { (m, i) =>
         m + (i -> (()), i + size -> (()))
       }
     }
   }
 
   measure method "+(separate args using fold)" in {
-    using (sizes) in { size =>
+    using(sizes) in { size =>
       (1 to size).foldLeft(Map.empty[Int, Unit]) { (m, i) =>
         Seq(i -> (()), i + size -> (())).foldLeft(m)(_ + _)
       }
